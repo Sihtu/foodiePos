2bc617e02c8043baee8a0b84e39527a51be23cd0
unknown <sithuzaw1800@gmail.com>
2024-07-23 23:00:28 +0630

    d

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,8 +3,8 @@ import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
-import Layout from "../components/Layout";
 import ThemeProviderWrapper from "../components/ThemeProviderWrapper";
+import Layout from "../components/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
