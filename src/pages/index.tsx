ece1fa4df53409bbdc0b243380efd95f45ebfa32
unknown <sithuzaw1800@gmail.com>
2024-08-08 22:39:16 +0630

    chage

---
+++ src/pages/index.tsx
@@ -0,0 +1,22 @@
+import { Box, Typography } from "@mui/material";
+import Link from "next/link";
+
+const Home = () => {
+  return (
+    <Box>
+
+      <Typography variant="h4">Landing Site(Stagings)</Typography>
+
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <Link href={"/backoffice"}>
+          <Typography variant="h5">Back Office</Typography>
+        </Link>
+        <Link href={"/order"}>
+          <Typography variant="h5">Order</Typography>
+        </Link>
+      </Box>
+    </Box>
+  );
+};
+
+export default Home;
